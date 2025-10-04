a3da5fe55112b0d45a085f286525aa759a08bfac
Akash6790 <akashuk679@gmail.com>
2025-10-05 01:42:01 +0530

    working of the navbar link corectly

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-
 import './App.css';
 import News from './components/News';
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-  <div>
-    <Navbar/>
-     <News pageSize={6} country="us" category="sports"/>
-  </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<News key="general" pageSize={6} country="us" category="general" />} />
+        <Route path="/business" element={<News key="business" pageSize={6} country="us" category="business" />} />
+        <Route path="/entertainment" element={<News key="entertainment" pageSize={6} country="us" category="entertainment" />} />
+        <Route path="/general" element={<News key="general" pageSize={6} country="us" category="general" />} />
+        <Route path="/health" element={<News key="health" pageSize={6} country="us" category="health" />} />
+        <Route path="/science" element={<News key="science" pageSize={6} country="us" category="science" />} />
+        <Route path="/sports" element={<News key="sports" pageSize={6} country="us" category="sports" />} />
+        <Route path="/technology" element={<News key="technology" pageSize={6} country="us" category="technology" />} />
+        <Route path="/about" element={<News key="about" pageSize={6} country="us" category="general" />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,52 +1,55 @@
 import React, { Component } from 'react'
+// import { Router } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export class Navbar extends Component {
   render() {
     return (
       <div>
+
         <nav className="navbar navbar-expand-lg navbar-primary bg-primary ">
-  <div className="container-fluid">
-    <a className="navbar-brand text-white" href="/">NewsAPI</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active text-white" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link text-white" href="/about">About</a>
-        </li> 
-     
-     
-            <li className="nav-item"><a className="nav-link text-white" href="/about">Business</a> </li>
-             <li className="nav-item">
-          <a className="nav-link text-white" href="/about">Entertainment</a>
-        </li>
-             <li className="nav-item">
-          <a className="nav-link text-white" href="/about">General</a>
-        </li>
-             <li className="nav-item">
-          <a className="nav-link text-white" href="/about">Health</a>
-        </li>
-             <li className="nav-item">
-          <a className="nav-link text-white" href="/about">Science</a>
-        </li>
-             <li className="nav-item">
-          <a className="nav-link text-white" href="/about">Sports</a>
-        </li>
-             <li className="nav-item">
-          <a className="nav-link text-white" href="/about">Technology</a>
-        </li>
-        </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success bg-warning text-white" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+          <div className="container-fluid">
+            <Link className="navbar-brand text-white" to="/">NewsAPI</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <Link className="nav-link active text-white" aria-current="page" to="/">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link text-white" to="/about">About</Link>
+                </li>
+                <li className="nav-item"><Link className="nav-link text-white" to="/business">Business</Link> </li>
+                <li className="nav-item">
+                  <Link className="nav-link text-white" to="/entertainment">Entertainment</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link text-white" to="/general">General</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link text-white" to="/health">Health</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link text-white" to="/science">Science</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link text-white" to="/sports">Sports</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link text-white" to="/technology">Technology</Link>
+                </li>
+              </ul>
+              <form className="d-flex">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-success bg-warning text-white" type="submit">Search</button>
+              </form>
+            </div>
+          </div>
+        </nav>
       </div>
     )
   }
